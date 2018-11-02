6645da3e264b50e90d04166ff5e42dbb0c81c046
Inc <38887202+WyInck@users.noreply.github.com>
2018-11-03 03:42:04 +0800

    Create enUS.lua

---
+++ Locales/enUS.lua
@@ -0,0 +1,12 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("LootWhisper", "enUS")
+if not L then return end
+L['LootWhisper'] = true
+L['LIVE'] = true
+L['ClickForIt'] = true
+L['CloseReset'] = true
+L['Hi, Do U need the'] = true
+L['? I really need it if U dont, ty! '] = true
+L['Welcome to use the LootWhisper, there is some tips you need to know:'] = true
+L['EQUIPMENTS - Left/Right Clicked will send the whisper message to the owner.'] = true
+L['RESET AND CLOESED - It will reset and close the menu if you leftclicked the close button.'] = true
+L['RESET ONLY - It only reset if you rightclicked the close button.'] = true
