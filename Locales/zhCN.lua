8c9719077ff68f83e8d0d867fb3165627f554def
Inc <38887202+WyInck@users.noreply.github.com>
2019-12-11 20:21:59 +0800

    Update zhCN.lua

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -1,12 +1,9 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("LootWhisper", "zhCN")
 if not L then return end
-L['LootWhisper'] = '毛装助手'
-L['ClickForIt'] = '点击装备毛装'
-L['CloseReset'] = '点击关闭重置'
+L['LootWhisper 8.3.0 Retail'] = '毛装助手 8.3.0 正式服'
 L['Hi, Do U need the'] = '大佬你好，这东西'
 L['? I really need it if U dont, ty!'] = '你要吗？不要的话，我跪求一发，非常感谢！'
 L['Welcome to use the LootWhisper, there is some tips you need to know:'] = '欢迎使用毛装助手，说明如下：'
 L['EQUIPMENTS - Left/Right Clicked will send the whisper message to the owner.'] = '毛装 - 左/右键单击装备即可向装备所有者发送密语。'
 L['RESET AND CLOESED - It will reset and close the menu if you leftclicked the close button.'] = '重置且关闭 - 左键点击关闭按钮。'
 L['RESET ONLY - It only reset if you rightclicked the close button.'] = '仅重置 - 右键点击关闭按钮。'
-L['RETAIL'] = '正式服'
