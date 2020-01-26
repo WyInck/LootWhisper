db45a4351a53eeeefb0002c5c8fa012f34362bd1
Inc <38887202+WyInck@users.noreply.github.com>
2020-01-26 18:08:31 +0800

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,8 +1,8 @@
 config = {
-	MAX_LOOTS = 5,
-	SHOW_SELVES = true,   
-	MIN_QUALITY = 0,   
-	EQUIP_ONLY = false,
-	SAME_ARMOR = true,
-	ILV_FILTER = true,
+	MAX_LOOTS = 10, --显示条目数量
+	SHOW_SELVES = false, --是否显示自身拾取 true/false 是/否
+	MIN_QUALITY = 3, --过滤品质， 0垃圾以上，1白色以上，2绿色以上，3蓝色以上，4史诗以上
+	EQUIP_ONLY = false, --是否仅显示装备
+	SAME_ARMOR = false, --是否仅显示同甲装备 true/false 是/否
+	ILV_FILTER = false, --是否启用装等过滤，默认不高于自身5装等的装备，不显示 true/false 是/否
 }
