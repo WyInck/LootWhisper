f431f3e9d651916fbd6f0a93fe883f92d64f32ae
Inc <38887202+WyInck@users.noreply.github.com>
2020-01-27 08:13:36 +0800

    Update enUS.lua

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -3,8 +3,8 @@ if not L then return end
 L['LootWhisper 8.3.0 Retail'] = true
 L['Hi, Do U need the'] = true
 L['? I really need it if U dont, ty!'] = true
-L['Welcome to use the LootWhisper, there is some tips you need to know:'] = true
+L['Welcome to use the lootwhisper, theres some tips u need to know:'] = true
 L['EQUIPMENTS - Left/Right Clicked will send the whisper message to the owner.'] = true
 L['RESET AND CLOESED - It will reset and close the menu if you leftclicked the close button.'] = true
 L['RESET ONLY - It only reset if you rightclicked the close button.'] = true
-L['LOOtWHISPER TIPS:/lw to begin'] = true
+L['/lw for more info.'] = true
